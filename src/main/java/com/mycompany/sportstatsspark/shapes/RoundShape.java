6c8d55b4a5ec0ffde82187bb4f06a493ee2e904a
mattkranc <mattkranc@130.243.9.140>
2018-04-05 10:58:15 +0200

    Added basic round service

---
+++ src/main/java/com/mycompany/sportstatsspark/shapes/RoundShape.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.sportstatsspark.shapes;
+
+/**
+ *
+ * @author mattkranc
+ */
+public class RoundShape {
+    public Long seasonId;
+    
+    public RoundShape(){
+        
+    }
+}
