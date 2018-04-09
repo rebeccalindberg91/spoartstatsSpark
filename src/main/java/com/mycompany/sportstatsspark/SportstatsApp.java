d57e13098c14110d3f195ca9144795189344022f
Erijoda <erijoda1991@gmail.com>
2018-04-09 14:26:21 +0200

    Added the ability to add result to a game.

--- src/main/java/com/mycompany/sportstatsspark/SportstatsApp.java
+++ src/main/java/com/mycompany/sportstatsspark/SportstatsApp.java
@@ -17,8 +17,9 @@ import java.util.Map;
 import static spark.Spark.*;
 import spark.servlet.SparkApplication;
 import sportstats.rest.json.JsonOutputFormatter;
-import sportstats.rest.shapes.GameShape;
+import sportstats.rest.shapes.ResultShape;
 import sportstats.service.AddLeagueService;
+import sportstats.service.AddResultService;
 import sportstats.service.AddRoundService;
 import sportstats.service.AddSeasonService;
 import sportstats.service.AddSportService;
@@ -45,12 +46,12 @@ public class SportstatsApp implements SparkApplication {
         before((req, res) -> {
             res.type("application/json");
         });
-        
+
         notFound((req, res) -> {
             res.status(404);
             return createError(404, "404 Not Found");
         });
-        
+
         internalServerError((req, res) -> {
             res.status(500);
             return createError(500, "500 Internal Server Error");
@@ -71,7 +72,7 @@ public class SportstatsApp implements SparkApplication {
                 return createError(ex.getMessage());
             }
         });
-        
+
         //Leagues
         get("/sports/:id/leagues", (req, res) -> {
             try {
@@ -87,7 +88,7 @@ public class SportstatsApp implements SparkApplication {
         post("/leagues", (req, res) -> {
             try {
                 LeagueShape newLeague = new Genson().deserialize(req.body(), LeagueShape.class);
-                
+
                 return run(new AddLeagueService(newLeague.name, newLeague.sportId));
             } catch (SportstatsServiceException ex) {
                 return createError(ex.getMessage());
@@ -95,7 +96,7 @@ public class SportstatsApp implements SparkApplication {
                 return createError("Wrong shape.");
             }
         });
-        
+
         //Seasons
         get("/leagues/:id/seasons", (req, res) -> {
             try {
@@ -111,7 +112,7 @@ public class SportstatsApp implements SparkApplication {
         post("/seasons", (req, res) -> {
             try {
                 SeasonShape newSeason = new Genson().deserialize(req.body(), SeasonShape.class);
-                
+
                 return run(new AddSeasonService(newSeason.year, newSeason.summer, newSeason.leagueId));
             } catch (SportstatsServiceException ex) {
                 return createError(ex.getMessage());
@@ -123,7 +124,7 @@ public class SportstatsApp implements SparkApplication {
         post("/seasons/:id/teams", (req, res) -> {
             try {
                 SeasonTeamShape newSeasonTeam = new Genson().deserialize(req.body(), SeasonTeamShape.class);
-                
+
                 return run(new AddTeamToSeasonService(newSeasonTeam.teamId,
                         newSeasonTeam.seasonId));
             } catch (SportstatsServiceException ex) {
@@ -132,7 +133,7 @@ public class SportstatsApp implements SparkApplication {
                 return createError("Wrong shape.");
             }
         });
-        
+
         //Teams
         get("/sports/:id/teams", (req, res) -> {
             try {
@@ -145,11 +146,11 @@ public class SportstatsApp implements SparkApplication {
                 return createError("SportId should be an integer");
             }
         });
-        
+
         post("/teams", (req, res) -> {
             try {
                 TeamShape newTeam = new Genson().deserialize(req.body(), TeamShape.class);
-                
+
                 return run(new AddTeamService(newTeam.name, newTeam.sportId));
             } catch (SportstatsServiceException ex) {
                 return createError(ex.getMessage());
@@ -157,52 +158,73 @@ public class SportstatsApp implements SparkApplication {
                 return createError("Wrong shape.");
             }
         });
-        
+
         //Rounds
         post("/rounds", (req, res) -> {
-          try {
-              RoundShape newRound = new Genson().deserialize(req.body(), RoundShape.class);
-              
-              return run(new AddRoundService(newRound.seasonId, newRound.games));
-          }  catch (SportstatsServiceException ex) {
-              return createError(ex.getMessage());
-          }  catch (Exception ex) {
-              return createError("Wrong shape.");
-          }
+            try {
+                RoundShape newRound = new Genson().deserialize(req.body(), RoundShape.class);
+
+                return run(new AddRoundService(newRound.seasonId, newRound.games));
+            } catch (SportstatsServiceException ex) {
+                return createError(ex.getMessage());
+            } catch (Exception ex) {
+                return createError("Wrong shape.");
+            }
         });
-        
+
         //Games
         get("/rounds/:id/games", (req, res) -> {
-           try {
-              return run(new GetGamesByRoundIdService(
-                               Long.valueOf(req.params(":id"))));
-           } catch (NumberFormatException ex) {
-               return createError("RoundId should be an integer");
-           }
+            try {
+                return run(new GetGamesByRoundIdService(
+                        Long.valueOf(req.params(":id"))));
+            } catch (NumberFormatException ex) {
+                return createError("RoundId should be an integer");
+            }
+        });
+
+        //Result
+        post("/games/:id/result", (req, res) -> {
+            try {
+                ResultShape newResult = new Genson().deserialize(req.body(), ResultShape.class);
+
+                return run(
+                        new AddResultService(newResult.gameId,
+                                newResult.scoreHomeTeam,
+                                newResult.scoreAwayTeam)
+                );
+            } catch (NumberFormatException ex) {
+                return createError("GameId should be an integer");
+            } catch (SportstatsServiceException ex) {
+                return createError(ex.getMessage());
+            } catch (Exception ex) {
+                return createError("Wrong shape.");
+            }
         });
     }
-    
-    
+
     private String run(SportstatsService service) {
         return new ServiceRunner<>(service).execute();
     }
-    
 
     private String createError(Integer statusCode, String message) {
-        Map<String, Object> errorContent = new HashMap<String, Object>() {{
-            if (statusCode != null) {
-                put("status", statusCode);
+        Map<String, Object> errorContent = new HashMap<String, Object>() {
+            {
+                if (statusCode != null) {
+                    put("status", statusCode);
+                }
+                put("message", message);
             }
-            put("message", message);
-        }};
-        Map<String, Object> error = new HashMap<String, Object>() {{
-            put("error", errorContent);
-        }};
-        
+        };
+        Map<String, Object> error = new HashMap<String, Object>() {
+            {
+                put("error", errorContent);
+            }
+        };
+
         return new JsonOutputFormatter()
                 .createOutput(error);
     }
-    
+
     private String createError(String message) {
         return createError(null, message);
     }
